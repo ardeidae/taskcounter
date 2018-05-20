42188d043c1939be1b9baf52f5d04bd90b022163
Matthieu PETIOT <matthieu.petiot@gmail.com>
2018-05-20 23:23:15 +0200

    Generate ts files for en and fr.

---
+++ resources/lang/en_US.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="40"/>
+        <source>About this software</source>
+        <translation>About this software</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="49"/>
+        <source>Source repository URL: {link}</source>
+        <translation>Source repository URL: {link}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="54"/>
+        <source>Author: {author}</source>
+        <translation>Author: {author}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="56"/>
+        <source>Version: {version}</source>
+        <translation>Version: {version}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="68"/>
+        <source>License</source>
+        <translation>License</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="69"/>
+        <source>Third-Party Software Notices</source>
+        <translation>Third-Party Software Notices</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="161"/>
+        <source>Year</source>
+        <translation>Year</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="173"/>
+        <source>Week</source>
+        <translation>Week</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="219"/>
+        <source>Week time</source>
+        <translation>Week time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="197"/>
+        <source>Man day time</source>
+        <translation>Man day time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="211"/>
+        <source>Week summary</source>
+        <translation>Week summary</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="222"/>
+        <source>Day time</source>
+        <translation>Day time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="226"/>
+        <source>Catch-up time</source>
+        <translation>Catch-up time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="272"/>
+        <source>Task counter</source>
+        <translation>Task counter</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="313"/>
+        <source>Go to {day}</source>
+        <translation>Go to {day}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="319"/>
+        <source>Previous Week</source>
+        <translation>Previous Week</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="323"/>
+        <source>Go to Previous Week</source>
+        <translation>Go to Previous Week</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="325"/>
+        <source>Next Week</source>
+        <translation>Next Week</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="328"/>
+        <source>Go to Next Week</source>
+        <translation>Go to Next Week</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="330"/>
+        <source>Today</source>
+        <translation>Today</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="333"/>
+        <source>Go to today</source>
+        <translation>Go to today</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="335"/>
+        <source>About</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="337"/>
+        <source>About this application</source>
+        <translation>About this application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="341"/>
+        <source>About Qt</source>
+        <translation>About Qt</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="343"/>
+        <source>Preferences</source>
+        <translation>Preferences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="346"/>
+        <source>Edit preferences</source>
+        <translation>Edit preferences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="348"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quit</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="350"/>
+        <source>Quit application</source>
+        <translation>Quit application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="353"/>
+        <source>Export</source>
+        <translation>Export</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="355"/>
+        <source>Export week summary as html table</source>
+        <translation>Export week summary as html table</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="370"/>
+        <source>Application</source>
+        <translation>Application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="376"/>
+        <source>Weeks</source>
+        <translation>Weeks</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="382"/>
+        <source>Days</source>
+        <translation>Days</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="606"/>
+        <source>Task</source>
+        <translation>Task</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="610"/>
+        <source>Time</source>
+        <translation>Time</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="39"/>
+        <source>Edit preferences</source>
+        <translation>Edit preferences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="48"/>
+        <source>Default week time</source>
+        <translation>Default week time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="55"/>
+        <source>Default man day time</source>
+        <translation>Default man day time</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="64"/>
+        <source>Invalid color</source>
+        <translation>Invalid color</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="75"/>
+        <source>Text</source>
+        <translation>Text</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="69"/>
+        <source>Valid color</source>
+        <translation>Valid color</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="74"/>
+        <source>Current cell color</source>
+        <translation>Current cell color</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="131"/>
+        <source>Select invalid color</source>
+        <translation>Select invalid color</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="145"/>
+        <source>Select current cell color</source>
+        <translation>Select current cell color</translation>
+    </message>
+</context>
+</TS>
---
+++ resources/lang/fr_FR.ts
@@ -0,0 +1,233 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="40"/>
+        <source>About this software</source>
+        <translation>À propos de ce logiciel</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="49"/>
+        <source>Source repository URL: {link}</source>
+        <translation>URL des sources : {link}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="54"/>
+        <source>Author: {author}</source>
+        <translation>Auteur : {author}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="56"/>
+        <source>Version: {version}</source>
+        <translation>Version : {version}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="68"/>
+        <source>License</source>
+        <translation>Licence</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/aboutdialog.py" line="69"/>
+        <source>Third-Party Software Notices</source>
+        <translation>Avis relatifs aux logiciels tiers</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="161"/>
+        <source>Year</source>
+        <translation>Année</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="173"/>
+        <source>Week</source>
+        <translation>Semaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="219"/>
+        <source>Week time</source>
+        <translation>Temps de la semaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="197"/>
+        <source>Man day time</source>
+        <translation>Temps de jour homme</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="211"/>
+        <source>Week summary</source>
+        <translation>Résumé de la semaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="222"/>
+        <source>Day time</source>
+        <translation>Temps du jour</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="226"/>
+        <source>Catch-up time</source>
+        <translation>Temps de rattrapage</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="272"/>
+        <source>Task counter</source>
+        <translation>Compteur de tâches</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="313"/>
+        <source>Go to {day}</source>
+        <translation>Aller à {day}</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="319"/>
+        <source>Previous Week</source>
+        <translation>Semaine précédente</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="323"/>
+        <source>Go to Previous Week</source>
+        <translation>Aller à la semaine précédente</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="325"/>
+        <source>Next Week</source>
+        <translation>Semaine prochaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="328"/>
+        <source>Go to Next Week</source>
+        <translation>Aller à la semaine prochaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="330"/>
+        <source>Today</source>
+        <translation>Aujourd&apos;hui</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="333"/>
+        <source>Go to today</source>
+        <translation>Aller à aujourd&apos;hui</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="335"/>
+        <source>About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="337"/>
+        <source>About this application</source>
+        <translation>À propos de cette application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="341"/>
+        <source>About Qt</source>
+        <translation>À propos de Qt</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="343"/>
+        <source>Preferences</source>
+        <translation>Préférences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="346"/>
+        <source>Edit preferences</source>
+        <translation>Modifier les préférences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="348"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="350"/>
+        <source>Quit application</source>
+        <translation>Quitter l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="353"/>
+        <source>Export</source>
+        <translation>Exporter</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="355"/>
+        <source>Export week summary as html table</source>
+        <translation>Exporter le résumé de la semaine en tant que tableau html</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="370"/>
+        <source>Application</source>
+        <translation>Application</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="376"/>
+        <source>Weeks</source>
+        <translation>Semaines</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="382"/>
+        <source>Days</source>
+        <translation>Jours</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="606"/>
+        <source>Task</source>
+        <translation>Tâche</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/mainwindow.py" line="610"/>
+        <source>Time</source>
+        <translation>Temps</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="39"/>
+        <source>Edit preferences</source>
+        <translation>Modifier les préférences</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="48"/>
+        <source>Default week time</source>
+        <translation>Temps par défaut de la semaine</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="55"/>
+        <source>Default man day time</source>
+        <translation>Temps par défaut du jour homme</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="64"/>
+        <source>Invalid color</source>
+        <translation>Couleur invalide</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="75"/>
+        <source>Text</source>
+        <translation>Texte</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="69"/>
+        <source>Valid color</source>
+        <translation>Couleur valide</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="74"/>
+        <source>Current cell color</source>
+        <translation>Couleur de la cellule courante</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="131"/>
+        <source>Select invalid color</source>
+        <translation>Selectionner la couleur invalide</translation>
+    </message>
+    <message>
+        <location filename="../../taskcounter/gui/settingdialog.py" line="145"/>
+        <source>Select current cell color</source>
+        <translation>Selectionner la couleur de la cellule courante</translation>
+    </message>
+</context>
+</TS>
